7c1a83037bf15d0656a1d40a380b5962eb125a8d
ndrdul <devdrdul@protonmail.com>
2022-05-22 16:10:16 -0400

    working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,54 @@
-import React from "react";
-import { NavbarContainer } from "../styles/Navbar.style";
+import React, { useState } from "react";
+
+import {
+  NavbarContainer,
+  LeftContainer,
+  RightContainer,
+  NavbarInnerContainer,
+  NavbarExtendedContainer,
+  NavbarLinkContainer,
+  NavbarLink,
+  Logo,
+  OpenLinksButton,
+  NavbarLinkExtended
+} from "../styles/Navbar.style";
+
+import LogoImage from "../assets/logo.png";
 
 export const Navbar = () => {
-  return <NavbarContainer></NavbarContainer>;
+  const [extendNavbar, setExtendNavbar] = useState(false);
+  return (
+    <NavbarContainer extendNavbar={extendNavbar}>
+      <NavbarInnerContainer>
+        <LeftContainer>
+          <NavbarLinkContainer>
+            <NavbarLink to="/">Home</NavbarLink>
+            <NavbarLink to="/products">Products</NavbarLink>
+            <NavbarLink to="/contact">Contact</NavbarLink>
+            <NavbarLink to="/about">About</NavbarLink>
+            <OpenLinksButton
+              onClick={() => {
+                setExtendNavbar((current) => !current);
+              }}
+            >
+              {extendNavbar ? <>&#10005;</> : <>&#8801;</>}
+            </OpenLinksButton>
+          </NavbarLinkContainer>
+        </LeftContainer>
+        <RightContainer>
+          <Logo src={LogoImage}></Logo>
+        </RightContainer>
+      </NavbarInnerContainer>
+      {extendNavbar && (
+        <NavbarExtendedContainer>
+          <NavbarLinkExtended to="/">Home</NavbarLinkExtended>
+          <NavbarLinkExtended to="/products">Products</NavbarLinkExtended>
+          <NavbarLinkExtended to="/contact">Contact</NavbarLinkExtended>
+          <NavbarLinkExtended to="/about">About</NavbarLinkExtended>
+        </NavbarExtendedContainer>
+      )}
+    </NavbarContainer>
+  );
 };
 
 export default Navbar;
